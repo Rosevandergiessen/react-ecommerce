bc3fbcf991f2fb29b5d33876a14f28d0c1330fd2
Rose van der Giessen <rosevandergiessen@proton.me>
2023-01-26 13:37:17 +0100

    set up redux store and root reducer

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore} from 'redux'
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+// library helpers that run before action hits reducer, dispatch - middleware - reducer
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+
+// depricated method, just for learning practices. Needs to be replaced by using redux-toolkit configuration.
+// optional second parameter
+export const store = createStore(rootReducer, undefined, composedEnhancers)
