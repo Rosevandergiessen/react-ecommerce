3bc7c32227d25f877e5099994cb2f4ba99dfe7d4
Rose van der Giessen <rosevandergiessen@proton.me>
2023-01-27 14:31:52 +0100

    added memoization with reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,9 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired')
+import { createSelector } from 'reselect';
 
-  return state.categories.categories.reduce((acc, category) => {
-  const { title, items } = category;
-  acc[title.toLowerCase()] = items;
-  return acc;
-}, {});
-};
+//Memoization if categories in categoriesreducer stays the same in memory, the cached value is enough (pure function)
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer], //input selector - slices from redux
+  (categoriesSlice) => categoriesSlice.categories//output selector
+)
+
+// As long as categoriesArray do not rerun reduce method - createSelector - create memoized selector
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,30 @@
 import { compose, applyMiddleware } from "redux";
 import { legacy_createStore as createStore} from 'redux'
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
 // library helpers that run before action hits reducer, dispatch - middleware - reducer
 
-const middleWares = [logger];
+//middleware function always follow same signature, 3 functions that return from one another:currying
+const loggerMiddleware = (store) => (next) => (action) => {
+  if(!action.type) {
+    return next(action);
+  }
+  console.log('type', action.type)
+  console.log('payload', action.payload)
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('next state: ', store.getState());
+}
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 
-// depricated method, just for learning practices. Needs to be replaced by using redux-toolkit configuration.
+// depricated method, just for learning purposes. Needs to be replaced by using redux-toolkit configuration.
 // optional second parameter
 export const store = createStore(rootReducer, undefined, composedEnhancers)
