652b1010e5e939448b3b6d0d72ea4328aef64e63
Rose van der Giessen <rosevandergiessen@proton.me>
2023-01-30 11:22:56 +0100

    added sign in and sign out sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,41 +1,44 @@
-import { Fragment} from "react";
-import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { selectIsCartOpen } from "../../store/cart/cart.selector";
-import { ReactComponent as Logo} from '../../assets/toshi-logo-green.svg';
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from '../../store/user/user.selector';
+import { signOutStart } from '../../store/user/user.action';
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
-import { selectCurrentUser } from "../../store/user/user.selector";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart());
 
   return (
     <Fragment>
       <NavigationContainer>
         <LogoContainer to='/'>
-          <Logo />
+          <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
-          <NavLink >
-            HOW IT WORKS
-          </NavLink>
-          <NavLink to='/shop'>
-            GAMES
-          </NavLink>
-          {
-            currentUser ? (
-              <NavLink  as='span' onClick={signOutUser}>SIGN OUT</NavLink>
-            ) : (<NavLink to='/auth'>
-            SIGN IN
-          </NavLink>
+          <NavLink to='/shop'>SHOP</NavLink>
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to='/auth'>SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
@@ -43,7 +46,7 @@ const Navigation = () => {
       </NavigationContainer>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
 export default Navigation;
